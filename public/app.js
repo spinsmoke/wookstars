1568bdd004f932f0632f36a76d0797a7e720f8d0
spinsmoke <34354007+spinsmoke@users.noreply.github.com>
2025-12-20 23:51:43 -0500

    Delete public directory

--- public/app.js
+++
@@ -1,7 +0,0 @@
-const uploadBtn = document.getElementById("uploadBtn");
-const status = document.getElementById("status");
-
-uploadBtn.addEventListener("click", () => {
-  status.textContent =
-    "Uploads coming soon 👀 Anonymous mode enabled.";
-});
